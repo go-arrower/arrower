09c387ca5965623f807a691ce99828828ea36a63
H <HTechHQ@users.noreply.github.com>
2023-05-01 17:35:31 +0200

    feat: update logger api to prevent globals and allow filtering for users #33

--- logger.go
+++ logger.go
@@ -1,6 +1,7 @@
 package arrower
 
 import (
+	"context"
 	"io"
 
 	"golang.org/x/exp/slog"
@@ -14,30 +15,100 @@ const (
 	LevelTrace = slog.Level(-12)
 )
 
-// SetLogLevel allows to set the global level for the loggers.
-func SetLogLevel(l slog.Level) {
-	arrowerLevel.Set(l)
-}
-
-// NewDevelopment provides a convenient logger for the development.
-func NewDevelopment(w io.Writer) *slog.Logger {
-	arrowerLevel.Set(slog.LevelDebug)
+func NewFilteredLogger(w io.Writer) *Handler {
+	level := slog.LevelDebug
 
 	opts := slog.HandlerOptions{
-		Level:       arrowerLevel,
+		Level:       &level,
 		AddSource:   false,
 		ReplaceAttr: nameArrowerLogLevels,
 	}.NewTextHandler(w)
 
-	return slog.New(opts)
+	filteredLogger := &filteredLogger{
+		orig:          opts,
+		observedUsers: map[string]struct{}{},
+	}
+
+	return &Handler{
+		Logger:       slog.New(filteredLogger),
+		filter:       filteredLogger,
+		arrowerLevel: &level,
+	}
+}
+
+type Handler struct {
+	Logger *slog.Logger
+	filter *filteredLogger
+
+	// arrowerLevel controls the level of the logger globally. Info by default.
+	arrowerLevel *slog.Level
+}
+
+// SetLogLevel allows to set the global level for the loggers.
+func (f *Handler) SetLogLevel(l slog.Level) {
+	*f.arrowerLevel = l
+}
+
+func (f *Handler) SetUserFilter(u string) {
+	f.filter.observedUsers[u] = struct{}{}
+}
+
+func (f *Handler) RemoveUserFilter(u string) {
+	delete(f.filter.observedUsers, u)
+}
+
+type filteredLogger struct {
+	orig          *slog.TextHandler
+	observedUsers map[string]struct{}
+}
+
+func (f *filteredLogger) Enabled(ctx context.Context, level slog.Level) bool {
+	enabled := f.orig.Enabled(ctx, level)
+
+	if enabled || len(f.observedUsers) > 0 {
+		return true
+	}
+
+	return false
+}
+
+func (f *filteredLogger) Handle(ctx context.Context, record slog.Record) error {
+	if f.orig.Enabled(ctx, record.Level) {
+		return f.orig.Handle(ctx, record) //nolint:wrapcheck // slog.Logger is exposed and used by the app developer.
+	}
+
+	var hasUserID bool
+
+	record.Attrs(func(attr slog.Attr) bool {
+		if attr.Key == "user_id" {
+			if _, ok := f.observedUsers[attr.Value.String()]; ok {
+				hasUserID = true
+
+				return false // stop iteration
+			}
+		}
+
+		return true // continue iteration
+	})
+
+	if hasUserID {
+		return f.orig.Handle(ctx, record) //nolint:wrapcheck // slog.Logger is exposed and used by the app developer.
+	}
+
+	return nil
+}
+
+func (f *filteredLogger) WithAttrs(attrs []slog.Attr) slog.Handler { //nolint:ireturn // is required to implement slog.
+	return f.orig.WithAttrs(attrs)
+}
+
+func (f *filteredLogger) WithGroup(name string) slog.Handler { //nolint:ireturn // is required to implement slog.
+	return f.orig.WithGroup(name)
 }
 
 //nolint:gochecknoglobals // recommended by slog documentation.
 var (
-	// arrowerLevel controls the level of the logger globally. Info by default.
-	arrowerLevel = new(slog.LevelVar)
-
-	// levelNames maps the arrower log levels to human readable names.
+	// levelNames maps the arrower log levels to human-readable names.
 	levelNames = map[slog.Leveler]string{
 		LevelDebug: "ARROWER:DEBUG",
 		LevelTrace: "ARROWER:TRACE",
--- logger_test.go
+++ logger_test.go
@@ -6,8 +6,15 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/exp/slog"
 
 	"github.com/go-arrower/arrower"
+)
+
+const (
+	userKey       = "user_id"
+	userName      = "1337"
+	userFormatted = "user_id=1337"
 )
 
 func TestNewDevelopment(t *testing.T) {
@@ -17,8 +24,8 @@ func TestNewDevelopment(t *testing.T) {
 		t.Parallel()
 
 		buf := &bytes.Buffer{}
-		logger := arrower.NewDevelopment(buf)
-		logger.Info("hello logger")
+		h := arrower.NewFilteredLogger(buf)
+		h.Logger.Info("hello logger")
 
 		assert.Contains(t, buf.String(), "hello logger")
 	})
@@ -27,13 +34,13 @@ func TestNewDevelopment(t *testing.T) {
 		t.Parallel()
 
 		buf := &bytes.Buffer{}
-		logger := arrower.NewDevelopment(buf)
+		h := arrower.NewFilteredLogger(buf)
 
-		logger.Log(context.Background(), arrower.LevelDebug, "arrower debug")
-		logger.Log(context.Background(), arrower.LevelTrace, "arrower trance")
+		h.Logger.Log(context.Background(), arrower.LevelDebug, "arrower debug")
+		h.Logger.Log(context.Background(), arrower.LevelTrace, "arrower trance")
 		assert.Empty(t, buf.String())
 
-		logger.Debug("application debug msg")
+		h.Logger.Debug("application debug msg")
 		assert.Contains(t, buf.String(), `msg="application debug msg"`)
 	})
 }
@@ -45,10 +52,10 @@ func TestSetLevel(t *testing.T) {
 		t.Parallel()
 
 		buf := &bytes.Buffer{}
-		logger := arrower.NewDevelopment(buf)
+		h := arrower.NewFilteredLogger(buf)
 
-		arrower.SetLogLevel(arrower.LevelDebug)
-		logger.Log(context.Background(), arrower.LevelDebug, "arrower debug")
+		h.SetLogLevel(arrower.LevelDebug)
+		h.Logger.Log(context.Background(), arrower.LevelDebug, "arrower debug")
 		assert.Contains(t, buf.String(), `msg="arrower debug"`)
 		assert.Contains(t, buf.String(), `level=ARROWER:DEBUG`)
 	})
@@ -57,11 +64,84 @@ func TestSetLevel(t *testing.T) {
 		t.Parallel()
 
 		buf := &bytes.Buffer{}
-		logger := arrower.NewDevelopment(buf)
+		h := arrower.NewFilteredLogger(buf)
 
-		arrower.SetLogLevel(arrower.LevelTrace)
-		logger.Log(context.Background(), arrower.LevelTrace, "arrower trace")
+		h.SetLogLevel(arrower.LevelTrace)
+		h.Logger.Log(context.Background(), arrower.LevelTrace, "arrower trace")
 		assert.Contains(t, buf.String(), `msg="arrower trace"`)
 		assert.Contains(t, buf.String(), `level=ARROWER:TRACE`)
 	})
+}
+
+func TestFilteredLogger_Handle(t *testing.T) {
+	t.Parallel()
+
+	buf := &bytes.Buffer{}
+	handler := arrower.NewFilteredLogger(buf)
+	logger := handler.Logger
+
+	buf.Reset()
+	logger.Debug("message")
+	assert.Contains(t, buf.String(), "message")
+
+	buf.Reset()
+	logger.Debug("", userKey, "default")
+	assert.Contains(t, buf.String(), "default", "not observed user")
+
+	handler.SetLogLevel(slog.LevelError)
+
+	buf.Reset()
+	logger.Debug("", userKey, "default")
+	assert.NotContains(t, buf.String(), "default", "not observed user")
+
+	buf.Reset()
+	logger.Debug("", userKey, userName)
+	assert.NotContains(t, buf.String(), userFormatted, "not observed yet")
+
+	buf.Reset()
+	handler.SetUserFilter("1337")
+	logger.Debug("", userKey, userName)
+	assert.Contains(t, buf.String(), userFormatted, "observed user")
+
+	buf.Reset()
+	handler.RemoveUserFilter(userName)
+	logger.Debug("", userKey, userName)
+	assert.NotContains(t, buf.String(), userFormatted, "removed user")
+
+	buf.Reset()
+	handler.SetLogLevel(slog.LevelDebug)
+
+	logger = logger.WithGroup("arrower")
+
+	handler.SetUserFilter(userName)
+	logger.Debug("", userKey, userName)
+	assert.Contains(t, buf.String(), userFormatted, "observed user with group")
+
+	buf.Reset()
+	handler.SetLogLevel(slog.LevelDebug)
+
+	logger = logger.With("app", "logger")
+	logger.Debug("", userKey, userName)
+	assert.Contains(t, buf.String(), "app=logger")
+}
+
+func TestFilteredLogger_Enabled(t *testing.T) {
+	t.Parallel()
+
+	buf := &bytes.Buffer{}
+	handler := arrower.NewFilteredLogger(buf)
+	logger := handler.Logger
+
+	handler.SetLogLevel(slog.LevelDebug)
+	logger.Debug("message", userKey, userName)
+	assert.Contains(t, buf.String(), userFormatted)
+
+	buf.Reset()
+	handler.SetLogLevel(slog.LevelError)
+	logger.Debug("message", userKey, userName)
+	assert.NotContains(t, buf.String(), userFormatted)
+
+	handler.SetUserFilter("1337")
+	logger.Debug("message", userKey, userName)
+	assert.Contains(t, buf.String(), userFormatted)
 }
