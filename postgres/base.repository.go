0052f9f6bbc7223f08fda05266ba2263a239f398
H <HTechHQ@users.noreply.github.com>
2023-06-30 18:13:28 +0900

    chore: try to fix linter issue in pipeline that does not occur locally, although the version is the same #36

--- postgres/base.repository.go
+++ postgres/base.repository.go
@@ -23,7 +23,7 @@ func NewPostgresBaseRepository[T interface{ WithTx(tx pgx.Tx) T }](
 
 // ConnOrTX wraps the models.Queries into the transaction in ctx.
 // If no transaction is in the context, it falls back to the raw Queries struct.
-func (repo BaseRepository[T]) ConnOrTX(ctx context.Context) T { //nolint:ireturn
+func (repo BaseRepository[T]) ConnOrTX(ctx context.Context) T { //nolint:ireturn,nolintlint // golangci-lint is bonkers
 	if tx, ok := ctx.Value(CtxTX).(pgx.Tx); ok {
 		return repo.queries.WithTx(tx)
 	}
@@ -34,7 +34,7 @@ func (repo BaseRepository[T]) ConnOrTX(ctx context.Context) T { //nolint:ireturn
 
 // TX wraps the models.Queries into the transaction in ctx.
 // If no transaction is present in the given context, it returns nil.
-func (repo BaseRepository[T]) TX(ctx context.Context) T { //nolint:ireturn
+func (repo BaseRepository[T]) TX(ctx context.Context) T { //nolint:ireturn,nolintlint // golangci-lint is bonkers
 	if tx, ok := ctx.Value(CtxTX).(pgx.Tx); ok {
 		return repo.queries.WithTx(tx)
 	}
@@ -45,6 +45,6 @@ func (repo BaseRepository[T]) TX(ctx context.Context) T { //nolint:ireturn
 }
 
 // Conn returns the models.Queries using the underlying db connection.
-func (repo BaseRepository[T]) Conn(ctx context.Context) T { //nolint:ireturn
+func (repo BaseRepository[T]) Conn(ctx context.Context) T { //nolint:ireturn,nolintlint // golangci-lint is bonkers
 	return repo.queries
 }
