dc475c7184b3a2b6b2a0d2271e03f2d30240a381
H <HTechHQ@users.noreply.github.com>
2023-12-09 22:33:14 +0100

    docs: improve documentation of repository unit tests

---
+++ tests/memory.example_extend_test.go
@@ -0,0 +1,50 @@
+package tests_test
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-arrower/arrower/tests"
+)
+
+func Example_extendRepositoryWithNewMethods() {
+	ctx := context.Background()
+
+	repo := NewUserMemoryRepository()
+	repo.Add(ctx, User{ID: 1, Login: "hello@arrower.org"})
+
+	u, _ := repo.FindByLogin(ctx, "hello@arrower.org")
+	fmt.Println(u)
+
+	// Output: {1 hello@arrower.org}
+}
+
+type UserID int
+
+type User struct {
+	ID    UserID
+	Login string
+}
+
+func NewUserMemoryRepository() *UserMemoryRepository {
+	return &UserMemoryRepository{
+		MemoryRepository: tests.NewMemoryRepository[User, UserID](),
+	}
+}
+
+type UserMemoryRepository struct {
+	*tests.MemoryRepository[User, UserID]
+}
+
+// FindByLogin implements a custom method, that is not supported by the tests.Repository out of the box.
+func (repo *UserMemoryRepository) FindByLogin(ctx context.Context, login string) (User, error) {
+	all, _ := repo.All(ctx)
+
+	for _, u := range all {
+		if u.Login == login {
+			return u, nil
+		}
+	}
+
+	return User{}, tests.ErrNotFound
+}
