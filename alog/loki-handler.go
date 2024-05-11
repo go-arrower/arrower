072916a70e0aa9bf08f0b849c6eeaeda03b5f36b
H <HTechHQ@users.noreply.github.com>
2024-05-11 16:09:08 +0200

    feat: allow loki logger to work, if loki is not available on startup

--- alog/loki-handler.go
+++ alog/loki-handler.go
@@ -5,7 +5,9 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/afiskon/promtail-client/promtail"
@@ -18,60 +20,119 @@ import (
 // It does not care about performance, as in production you would log to `stdout` and the
 // container-runtime's drivers (docker, kubernetes) or something will ship your logs to loki.
 func NewLokiHandler(opt *LokiHandlerOptions) *LokiHandler {
+	conf := getPromtailConfig(opt)
+	client := getClient(conf)
+
+	// generate json log by writing to local buffer with slog default json
+	buf := &bytes.Buffer{}
+	renderer := slog.NewJSONHandler(buf, &slog.HandlerOptions{
+		Level:       LevelDebug, // allow all messages, as the level gets controlled by the ArrowerLogger instead.
+		AddSource:   false,
+		ReplaceAttr: MapLogLevelsToName,
+	})
+
+	handler := &LokiHandler{
+		mu:       sync.Mutex{},
+		client:   client,
+		renderer: renderer,
+		output:   buf,
+	}
+
+	if client == nil {
+		go retryLokiConnection(handler, conf)
+	}
+
+	return handler
+}
+
+func getPromtailConfig(opt *LokiHandlerOptions) promtail.ClientConfig {
 	defaultOpt := &LokiHandlerOptions{
 		PushURL: "http://localhost:3100/api/prom/push",
-		Label:   fmt.Sprintf("{%s=\"%s\"}", "arrower", "skeleton"),
+		Labels: map[string]string{
+			"arrower": "application",
+			"client":  "arrower-loki",
+		},
 	}
 
 	if opt == nil {
 		opt = defaultOpt
 	}
 
-	if opt.PushURL != "" {
+	if opt.PushURL == "" {
 		opt.PushURL = defaultOpt.PushURL
 	}
 
-	if opt.Label != "" {
-		opt.Label = defaultOpt.Label
+	if len(opt.Labels) == 0 {
+		opt.Labels = defaultOpt.Labels
 	}
 
-	conf := promtail.ClientConfig{
+	label := "{"
+	for k, l := range opt.Labels {
+		label += fmt.Sprintf("%s=\"%s\",", k, l)
+	}
+	label += "}" //nolint:wsl
+
+	return promtail.ClientConfig{
 		PushURL:            opt.PushURL,
 		BatchWait:          1 * time.Second,
 		BatchEntriesNumber: 1,
 		SendLevel:          promtail.DEBUG,
 		PrintLevel:         promtail.DISABLE,
-		Labels:             opt.Label,
+		Labels:             label,
+	}
+}
+
+func retryLokiConnection(handler *LokiHandler, conf promtail.ClientConfig) {
+	const lokiRetryInterval = 15 * time.Second
+	t := time.NewTicker(lokiRetryInterval)
+
+	for range t.C {
+		client := getClient(conf)
+		if client == nil {
+			continue
+		}
+
+		handler.mu.Lock()
+		defer handler.mu.Unlock()
+
+		handler.client = client
+
+		return
+	}
+}
+
+func getClient(conf promtail.ClientConfig) promtail.Client { //nolint:ireturn,lll // promtail.NewClientX() only returns interface.
+	cli := &http.Client{} //nolint:exhaustruct
+
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, conf.PushURL, nil)
+	if err != nil {
+		slog.Info("could not get request", slog.Any("err", err), slog.String("url", conf.PushURL))
+		return nil
 	}
 
-	// Do not handle error here, because promtail method always returns `nil`.
-	client, _ := promtail.NewClientJson(conf)
-
-	// generate json log by writing to local buffer with slog default json
-	buf := &bytes.Buffer{}
-	jsonLog := slog.HandlerOptions{
-		Level:       LevelDebug, // allow all messages, as the level gets controlled by the ArrowerLogger instead.
-		AddSource:   false,
-		ReplaceAttr: MapLogLevelsToName,
+	res, err := cli.Do(req)
+	if err != nil {
+		slog.Info("could not ping loki", slog.Any("err", err), slog.String("url", conf.PushURL))
+		return nil
 	}
-	renderer := slog.NewJSONHandler(buf, &jsonLog)
 
-	return &LokiHandler{
-		client:   client,
-		renderer: renderer,
-		output:   buf,
-	}
+	_ = res.Body.Close()
+
+	client, _ := promtail.NewClientJson(conf) // Do not handle error here, because promtail always returns `nil`.
+
+	return client
 }
 
 type (
 	LokiHandlerOptions struct {
+		Labels  map[string]string
 		PushURL string
-		Label   string
 	}
 
-	// LokiHandler, see NewLokiHandler.
-	LokiHandler struct {
-		client   promtail.Client
+	LokiHandler struct { //nolint:govet // fieldalignment not as important as readability.
+		mu     sync.Mutex
+		client promtail.Client
+
 		renderer slog.Handler
 		output   *bytes.Buffer
 	}
@@ -80,7 +141,17 @@ type (
 var _ slog.Handler = (*LokiHandler)(nil)
 
 func (l *LokiHandler) Handle(ctx context.Context, record slog.Record) error {
-	_ = l.renderer.Handle(ctx, record)
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if l.client == nil { // client is empty if no loki instance is available => do not log.
+		return nil
+	}
+
+	err := l.renderer.Handle(ctx, record)
+	if err != nil {
+		return fmt.Errorf("%w", err)
+	}
 
 	var attrs string
 
@@ -118,6 +189,7 @@ func (l *LokiHandler) Enabled(_ context.Context, _ slog.Level) bool {
 
 func (l *LokiHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
 	return &LokiHandler{
+		mu:       sync.Mutex{},
 		client:   l.client,
 		renderer: l.renderer.WithAttrs(attrs),
 		output:   l.output,
@@ -126,6 +198,7 @@ func (l *LokiHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
 
 func (l *LokiHandler) WithGroup(name string) slog.Handler {
 	return &LokiHandler{
+		mu:       sync.Mutex{},
 		client:   l.client,
 		renderer: l.renderer.WithGroup(name),
 		output:   l.output,
